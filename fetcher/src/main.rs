7d07be698cbf8c60eea060ef2067ae805b41e04d
Thomas Schleicher <thscleicher@edu.aau.at>
2025-10-19 14:45:07 +0200

    fetcher clap impl

--- fetcher/src/main.rs
+++ fetcher/src/main.rs
@@ -1,18 +1,28 @@
+use clap::Parser;
+use futures::future::join_all;
 use reqwest::Client;
 
 mod fetch_measurement_data;
 mod io;
 
+#[derive(Debug, Parser)]
+struct Cli {
+    #[clap(short, long)]
+    measurements: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    let measurement_ids = io::read_measurement_ids_from_file(&args.measurements)?;
     let client = Client::new();
 
-    //TODO: read measurement config file path from command line argument using clap
+    let futures = measurement_ids
+        .ids
+        .iter()
+        .map(|id| fetch_measurement_data::get_measurement_data(&client, id));
 
-    let measurement_ids = match io::read_measurement_ids_from_file("") {
-        Ok(ids) => ids,
-        Err(error) => panic!("Error: {}", error),
-    };
+    let results = join_all(futures).await;
 
     let measurements =
         match fetch_measurement_data::get_measurement_data(&client, "121060141").await {
