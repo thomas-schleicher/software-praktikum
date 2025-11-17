db43506cbbcbb389e6ea7f30d796cc81843af70c
Thomas Schleicher <thschleicher@edu.aau.at>
2025-11-17 17:34:26 +0100

    wip

--- fetcher/src/api/results.rs
+++ fetcher/src/api/results.rs
@@ -8,7 +8,6 @@ pub enum AggregatedMeasurement {
     #[serde(rename = "ping")]
     Ping(PingMeasurement),
     #[serde(rename = "traceroute")]
-    //TODO: create a traceroute measurement and check the type parameter in results
     TraceRoute(TraceRouteMeasurement),
 }
 
@@ -99,5 +98,76 @@ impl FlattenedHttpMeasurement {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TraceRouteMeasurement {
-    //TODO: Create traceroute measurement
+    pub endtime: usize,
+    pub dst_addr: String,
+    pub src_addr: String,
+    pub proto: String,
+    pub paris_id: u32,
+    pub result: Vec<TraceRouteResult>,
+    pub destination_ip_responded: bool,
+    pub probe_id: String,
+    pub msm_id: String,
+    pub timestamp: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TraceRouteResult {
+    pub hop: u32,
+    pub result: Vec<HopResult>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HopResult {
+    pub from: Option<String>,
+    pub ttl: Option<u32>,
+    pub size: Option<u32>,
+    pub rtt: Option<f32>,
+    pub x: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FlattenedTraceRouteMeasurement {
+    pub msm_id: String,
+    pub hop: u32,
+    pub sent: u32,
+    pub recived: u32,
+    pub min: f32,
+    pub avg: f32,
+    pub max: f32,
+    pub from: String,
+    pub timed_out: bool,
+    pub timestamp: usize,
+    pub probe_id: String,
+    pub endtime: usize,
+    pub proto: String,
+    pub paris_id: u32,
+    pub destination_ip_responded: bool,
+}
+
+impl FlattenedTraceRouteMeasurement {
+    pub fn from_traceroute_measurement(
+        traceroute_measurement: &TraceRouteMeasurement,
+    ) -> Vec<Self> {
+        traceroute_measurement
+            .result
+            .iter()
+            .map(|result| FlattenedTraceRouteMeasurement {
+                msm_id: todo!(),
+                hop: todo!(),
+                sent: todo!(),
+                recived: todo!(),
+                min: todo!(),
+                avg: todo!(),
+                max: todo!(),
+                from: todo!(),
+                timed_out: todo!(),
+                timestamp: todo!(),
+                probe_id: todo!(),
+                endtime: todo!(),
+                proto: todo!(),
+                paris_id: todo!(),
+                destination_ip_responded: todo!(),
+            })
+            .collect()
+    }
 }
